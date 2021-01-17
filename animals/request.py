80c05a4411fbfab90cbe417e0c54d1a7b4eafc4f
negative1skunk@gmail.com <negative1skunk@gmail.com>
2021-01-17 10:01:37 -0600

    chapter 10 dun

--- animals/request.py
+++ animals/request.py
@@ -134,7 +134,58 @@ def get_single_animal(id):
                             data['customer_id'])
 
         return json.dumps(animal.__dict__)
-        
+
+def get_animals_by_location(location_id):
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.breed,
+            a.status,
+            a.location_id,
+            a.customer_id
+        FROM animal a
+        WHERE a.location_id = ?
+        """, ( location_id, ))
+
+        animals = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            animal = Animal(row['id'], row['name'], row['breed'],row['status'], row['location_id'],row['customer_id'])
+            animals.append(animal.__dict__)
+            
+    return json.dumps(animals)
+
+def get_animals_by_status(status):
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.breed,
+            a.status,
+            a.location_id,
+            a.customer_id
+        FROM animal a
+        WHERE a.status = ?
+        """, ( status, ))
+
+        animals = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            animal = Animal(row['id'], row['name'], row['breed'],row['status'], row['location_id'],row['customer_id'])
+            animals.append(animal.__dict__)
+    return json.dumps(animals)
+
 def create_animal(animal):
         # Get the id value of the last animal in the list
         max_id = ANIMALS[-1]["id"]
--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,13 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
+
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_animals_by_location, get_animals_by_status
+
 from locations import get_all_locations, get_single_location, create_location, delete_location, update_location
-from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
-from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
+
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee, get_employees_by_location
+
+from customers import get_all_customers, get_single_customer, get_customers_by_email, create_customer, delete_customer, update_customer
 
 
 
@@ -96,6 +100,12 @@ class HandleRequests(BaseHTTPRequestHandler):
             # email as a filtering value?
             if key == "email" and resource == "customers":
                 response = get_customers_by_email(value)
+            elif key == "location_id" and resource == "animals":
+                response = get_animals_by_location(value)
+            elif key == "status" and resource == "animals":
+                response = get_animals_by_status(value)
+            elif key == "location_id" and resource == "employees":
+                response = get_employees_by_location(value)
 
         self.wfile.write(response.encode())
 
