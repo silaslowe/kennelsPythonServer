e67ce642284127973fb5b53ef5122099933f51f1
negative1skunk@gmail.com <negative1skunk@gmail.com>
2021-01-19 10:27:29 -0600

    chapter 13 dun

--- request_handler.py
+++ request_handler.py
@@ -158,7 +158,6 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
     def do_PUT(self):
-        self._set_headers(204)
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
         post_body = json.loads(post_body)
@@ -168,16 +167,20 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         # Delete a single animal from the list
         if resource == "animals":
-            update_animal(id, post_body)
+            success = update_animal(id, post_body)
+        if success:
+            self._set_headers(204)
+        else:
+            self._set_headers(404)
         
-        if resource == "locations":
-            update_location(id, post_body)  
+        # if resource == "locations":
+        #     update_location(id, post_body)  
 
-        if resource == "customers":
-            update_customer(id, post_body)
+        # if resource == "customers":
+        #     update_customer(id, post_body)
 
-        if resource == "employees":
-            update_employee(id, post_body)  
+        # if resource == "employees":
+        #     update_employee(id, post_body)  
 
         # Encode the new animal and send in response
         self.wfile.write("".encode())
